9d1553b9b2975e8fc4d3e9eeda2069c1eef091fd
Scott Morrison <scott@tqft.net>
2019-09-03 22:02:06 +1000

    proof by Johan

--- src/topology/local.lean
+++ src/topology/local.lean
@@ -16,20 +16,12 @@ def local_pred {τ : Type*} (P : Π {α : Type*} [topological_space α], (α →
 /-- Continuity is a local predicate (on functions with any fixed target topological space). -/
 theorem continuous_local {τ : Type*} [topological_space τ] :
   local_pred (λ α _ (f : α → τ), by exactI continuous f) :=
-λ α _ f h,
+λ X _ f H,
 begin
   resetI,
-  intros s o,
-  -- For each `x`, we take a nbhd `U x` on which we're promised `f` is continuous.
-  let U := λ x : α, classical.some (h x),
-  -- We define `V x` to be the preimage of s under the restriction of `f` to `U x`.
-  let V' := λ x : α, (f ∘ (subtype.val : U x → α)) ⁻¹' s,
-  let V := λ x : α, subtype.val '' (V' x),
-  -- Now we show that each `V x` is open (being an open subset of `U x`, by the promised continuity).
-  have V_open : ∀ x, is_open (V x), sorry,
-  -- And finally that `f ⁻¹' s` is the union of the `V x`.
-  have eq_union : f ⁻¹' s = ⨆ x, V x, sorry,
-  -- Now it's just that unions of opens are open.
-  rw eq_union,
-  sorry
+  rw continuous_iff_continuous_at,
+  intros x V hV,
+  rcases H x with ⟨U, hx, h⟩,
+  convert h.tendsto ⟨x, hx⟩ hV using 2,
+  exact congr_arg (filter.map f) (map_nhds_subtype_val_eq _ (mem_nhds_sets U.2 hx)).symm,
 end
