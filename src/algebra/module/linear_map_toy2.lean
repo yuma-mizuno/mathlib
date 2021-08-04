b1970a6d01fa8bc828a60667ae25108b7b3af063
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2021-08-03 22:50:57 -0400

    make automorphism an out-param, doesn't help

--- src/algebra/module/linear_map_toy2.lean
+++ src/algebra/module/linear_map_toy2.lean
@@ -5,19 +5,15 @@ Authors: foo
 -/
 import algebra.group_action_hom
 
-variables {R : Type*} {S : Type*} {R₁ : Type*} {R₂ : Type*} {R₃ : Type*}
-variables [semiring R] [semiring S] [semiring R₁] [semiring R₂] [semiring R₃]
-variables {σ : R ≃+* S}
+variables {R : Type*} [semiring R] {σ : R ≃+* R} {σ' : out_param (R ≃+* R)}
 variables {M : Type*} {N : Type*} {M₁ : Type*} {M₂ : Type*} {M₃ : Type*}
 variables [add_comm_monoid M] [add_comm_monoid N] [add_comm_monoid M₁] [add_comm_monoid M₂]
   [add_comm_monoid M₃]
-variables [module R M] [module S N]
-
+variables [module R M] [module R N] [module R M₁] [module R M₂] [module R M₃]
 
 /-! ### The `ring_equiv_comp_triple` typeclass hack -/
 section
-variables [module R₁ M₁] [module R₂ M₂] [module R₃ M₃]
-variables (σ₁₂ : R₁ ≃+* R₂) (σ₂₃ : R₂ ≃+* R₃) (σ₁₃ : out_param (R₁ ≃+* R₃))
+variables (σ₁₂ : R ≃+* R) (σ₂₃ : R ≃+* R) (σ₁₃ : out_param (R ≃+* R))
 
 class ring_equiv_comp_triple : Prop :=
   (is_comp_triple : σ₁₃ = σ₁₂.trans σ₂₃)
@@ -25,8 +21,8 @@ variables {σ₁₂} {σ₂₃} {σ₁₃}
 
 namespace ring_equiv_comp_triple
 
-instance ids : ring_equiv_comp_triple (ring_equiv.refl R₁) σ₁₂ σ₁₂ := ⟨by { ext, simp }⟩
-instance right_ids : ring_equiv_comp_triple σ₁₂ (ring_equiv.refl R₂) σ₁₂ := ⟨by { ext, simp }⟩
+instance ids : ring_equiv_comp_triple (ring_equiv.refl R) σ₁₂ σ₁₂ := ⟨by { ext, simp }⟩
+instance right_ids : ring_equiv_comp_triple σ₁₂ (ring_equiv.refl R) σ₁₂ := ⟨by { ext, simp }⟩
 
 end ring_equiv_comp_triple
 
@@ -36,25 +32,24 @@ end
 /-! ### Linear maps -/
 section
 set_option old_structure_cmd true
-variables (σ M N)
+variables (R M N)
 
 /-- A map `f` between modules over a semiring is linear if it satisfies the two properties
 `f (x + y) = f x + f y` and `f (c • x) = c • f x`. -/
 structure linear_map extends add_hom M N :=
-(map_smul' : ∀ (r : R) (x : M), to_fun (r • x) = (σ r) • to_fun x)
+(map_smul' : ∀ (r : R) (x : M), to_fun (r • x) = (σ' r) • to_fun x)
 
 end
 
-notation M ` →ₛₗ[`:25 σ:25 `] `:0 N:0 := linear_map σ M N
-notation M ` →ₗ[`:25 R:25 `] `:0 N:0 := linear_map (ring_equiv.refl R) M N
+notation M ` →ₛₗ[`:25 σ:25 `] `:0 N:0 := @linear_map _ _ σ M N _ _ _ _
+notation M ` →ₗ[`:25 R:25 `] `:0 N:0 := @linear_map _ _ (ring_equiv.refl R) M N _ _ _ _
 
 /-! Define composition of semilinear maps. -/
 namespace linear_map
 
-instance : has_coe_to_fun (M →ₛₗ[σ] N) := ⟨_, to_fun⟩
+instance : has_coe_to_fun (M →ₛₗ[σ'] N) := ⟨_, to_fun⟩
 
-variables [module R₁ M₁] [module R₂ M₂] [module R₃ M₃]
-variables {σ₁₂ : R₁ ≃+* R₂} {σ₂₃ : R₂ ≃+* R₃} {σ₁₃ : out_param (R₁ ≃+* R₃)}
+variables {σ₁₂ : R ≃+* R} {σ₂₃ : R ≃+* R} {σ₁₃ : out_param (R ≃+* R)}
 
 def comp [ring_equiv_comp_triple σ₁₂ σ₂₃ σ₁₃] (f : M₂ →ₛₗ[σ₂₃] M₃) (g : M₁ →ₛₗ[σ₁₂] M₂) :
   M₁ →ₛₗ[σ₁₃] M₃ :=
@@ -69,27 +64,30 @@ end linear_map
 /-! ### Linear equivalences -/
 section
 set_option old_structure_cmd true
-variables (σ M N)
+variables (R M N)
 
+include σ'
 /-- A linear equivalence is an invertible linear map. -/
-structure linear_equiv extends linear_map σ M N, M ≃+ N
+structure linear_equiv extends add_equiv M N :=
+(map_smul' : ∀ (r : R) (x : M), to_fun (r • x) = (σ' r) • to_fun x)
 
 end
 
-notation M ` ≃ₛₗ[`:50 σ `] ` N := linear_equiv σ M N
-notation M ` ≃ₗ[`:50 R `] ` N := linear_equiv (ring_equiv.refl R) M N
+notation M ` ≃ₛₗ[`:50 σ `] ` N := @linear_equiv _ _ σ M N _ _ _ _
+notation M ` ≃ₗ[`:50 R `] ` N := @linear_equiv _ _ (ring_equiv.refl R) M N _ _ _ _
 
 /-! Define coercion -/
 namespace linear_equiv
 
-instance has_coe : has_coe (M ≃ₛₗ[σ] N) (M →ₛₗ[σ] N) := ⟨to_linear_map⟩
+def to_linear_map (e : M ≃ₛₗ[σ'] N) : M →ₛₗ[σ'] N := { ..e }
+
+instance has_coe : has_coe (M ≃ₛₗ[σ'] N) (M →ₛₗ[σ'] N) := ⟨to_linear_map⟩
 
 end linear_equiv
 
 
 /-! ### The elaboration issue! -/
 section
-variables [module R M₁] [module R M₂] [module R M₃]
 variables (f₂₃ : M₂ →ₗ[R] M₃) (e₁₂ : M₁ ≃ₗ[R] M₂)
 
 -- Ex. 1, works
@@ -112,6 +110,6 @@ example : M₁ →ₗ[R] M₃ := f₂₃.comp (e₁₂ : M₁ →ₗ[R] M₂)
 
 -- Ex. 6, works
 example : M₁ →ₗ[R] M₃ :=
-@linear_map.comp _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ ring_equiv_comp_triple.ids f₂₃ ↑e₁₂
+@linear_map.comp _ _ _ _ _ _ _ _ _ _ _ _ _ _ ring_equiv_comp_triple.ids f₂₃ ↑e₁₂
 
 end
