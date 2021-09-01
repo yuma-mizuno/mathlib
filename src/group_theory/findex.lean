5ddf143dc432175006c75d28e4fd7b6d44ab53de
Thomas Browning <tb65536@uw.edu>
2021-09-01 12:29:24 -0700

    findex stuff

--- src/group_theory/findex.lean
+++ src/group_theory/findex.lean
@@ -1,31 +1,9 @@
-import linear_algebra.finite_dimensional
 import group_theory.coset
-
-lemma cardinal.to_nat_mul (x y : cardinal) : (x * y).to_nat = x.to_nat * y.to_nat :=
-begin
-  by_cases hx1 : x = 0,
-  { rw [hx1, zero_mul, cardinal.zero_to_nat, zero_mul] },
-  by_cases hy1 : y = 0,
-  { rw [hy1, mul_zero, cardinal.zero_to_nat, mul_zero] },
-  cases lt_or_ge x cardinal.omega with hx2 hx2,
-  { cases lt_or_ge y cardinal.omega with hy2 hy2,
-    { rw [cardinal.to_nat_apply_of_lt_omega (cardinal.mul_lt_omega hx2 hy2),
-          cardinal.to_nat_apply_of_lt_omega hx2, cardinal.to_nat_apply_of_lt_omega hy2,
-          ←cardinal.nat_cast_inj, nat.cast_mul,
-          ←classical.some_spec (cardinal.lt_omega.mp (cardinal.mul_lt_omega hx2 hy2)),
-          ←classical.some_spec (cardinal.lt_omega.mp hx2),
-          ←classical.some_spec (cardinal.lt_omega.mp hy2)] },
-    { rw [cardinal.to_nat_apply_of_omega_le hy2, mul_zero, cardinal.to_nat_apply_of_omega_le],
-      rw [←not_lt, cardinal.mul_lt_omega_iff_of_ne_zero hx1 hy1, not_and_distrib, not_lt, not_lt],
-      exact or.inr hy2 } },
-  { rw [cardinal.to_nat_apply_of_omega_le hx2, zero_mul, cardinal.to_nat_apply_of_omega_le],
-    rw [←not_lt, cardinal.mul_lt_omega_iff_of_ne_zero hx1 hy1, not_and_distrib, not_lt, not_lt],
-    exact or.inl hx2 },
-end
+import set_theory.cardinal
 
 namespace subgroup
 
-variables {G : Type*} [group G] (H K : subgroup G)
+variables {G : Type*} [group G] (H : subgroup G)
 
 /-- The index of a subgroup as a natural number -/
 noncomputable def findex : ℕ :=
@@ -48,15 +26,43 @@ begin
   exact H.findex_eq_card,
 end
 
-variables {H K}
+lemma key_lemma1 (g : G) :
+  ∃ h : H, (quotient_group.mk g : quotient_group.quotient H).out' = g * h :=
+⟨⟨g⁻¹ * (quotient_group.mk g).out', quotient_group.eq'.mp (quotient_group.mk g).out_eq'.symm⟩,
+  by rw [H.coe_mk, mul_inv_cancel_left]⟩
+
+variables {H}
+
+lemma key_lemma2 (g₁ g₂ : G) (hg₂ : g₂ ∈ H) :
+  (quotient_group.mk (g₁ * g₂) : quotient_group.quotient H) = quotient_group.mk g₁ :=
+by rwa [quotient_group.eq', mul_inv_rev, inv_mul_cancel_right, H.inv_mem_iff]
+
+variables {K : subgroup G}
+
+noncomputable def tada (h_le : H ≤ K) : quotient_group.quotient H ≃
+  quotient_group.quotient K × quotient_group.quotient (H.subgroup_of K) :=
+{ to_fun := λ a, ⟨quotient_group.mk a.out', quotient_group.mk
+    ⟨(quotient_group.mk a.out' : quotient_group.quotient K).out'⁻¹ * a.out',
+      by rw [←quotient_group.eq', quotient_group.out_eq']⟩⟩,
+  inv_fun := λ a, quotient_group.mk (a.1.out' * a.2.out'),
+  left_inv := λ a, by
+  { obtain ⟨x₁, h₁⟩ := K.key_lemma1 a.out',
+    obtain ⟨x₂, h₂⟩ := (H.subgroup_of K).key_lemma1 x₁⁻¹,
+    simp_rw [h₁, mul_inv_rev, inv_mul_cancel_right, ←K.coe_inv, subtype.coe_eta],
+    simp_rw [h₂, K.coe_mul, K.coe_inv, mul_assoc, mul_inv_cancel_left],
+    exact (key_lemma2 a.out' x₂ x₂.2).trans a.out_eq' },
+  right_inv := λ a, by
+  { obtain ⟨x, h⟩ := H.key_lemma1 (a.1.out' * a.2.out'),
+    simp_rw [h, mul_assoc, key_lemma2 a.1.out' (a.2.out' * x) (K.mul_mem a.2.out'.2 (h_le x.2)),
+      quotient_group.out_eq', inv_mul_cancel_left],
+    change (a.1, quotient_group.mk (⟨(a.2.out' * ⟨x, h_le x.2⟩ : K), _⟩ : K)) = a,
+    simp_rw subtype.coe_eta,
+    exact prod.ext rfl ((key_lemma2 a.2.out' ⟨x, h_le x.2⟩ (by exact x.2)).trans a.2.out_eq') } }
 
 lemma findex_dvd_of_le (h_le : H ≤ K) : K.findex ∣ H.findex :=
 begin
-  use (cardinal.mk (quotient_group.quotient (H.subgroup_of K))).to_nat,
-  rw [findex, findex, ←cardinal.to_nat_mul],
-  apply congr_arg cardinal.to_nat,
-  apply cardinal.eq_congr,
-  sorry,
+  refine ⟨(H.subgroup_of K).findex, eq.trans _ (cardinal.to_nat_mul _ _)⟩,
+  exact congr_arg cardinal.to_nat (cardinal.eq_congr (tada h_le)),
 end
 
 end subgroup
