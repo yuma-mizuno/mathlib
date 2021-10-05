dbaa17b36fdabdb896f62527fb33a3cd27577813
RemyDegenne <Remydegenne@gmail.com>
2021-10-05 18:02:19 +0200

    fix imports

--- src/analysis/calculus/specific_functions.lean
+++ src/analysis/calculus/specific_functions.lean
@@ -4,7 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Sébastien Gouëzel
 -/
 import analysis.calculus.iterated_deriv
+import analysis.calculus.extend_deriv
 import analysis.inner_product_space.euclidean_dist
+import analysis.special_functions.exp_log_calculus
 
 /-!
 # Infinitely smooth bump function
