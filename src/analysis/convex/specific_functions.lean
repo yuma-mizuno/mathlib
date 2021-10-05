bc09898fabb811d5da516a854aff52eaa98a14e7
RemyDegenne <Remydegenne@gmail.com>
2021-10-05 18:19:41 +0200

    delete empty file

--- src/analysis/convex/specific_functions.lean
+++
@@ -1,9 +0,0 @@
-/-
-Copyright (c) 2020 Yury Kudryashov. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Yury Kudryashov, Sébastien Gouëzel
--/
-import analysis.convex.specific_functions.pow
-
-open real set
-open_locale big_operators nat
