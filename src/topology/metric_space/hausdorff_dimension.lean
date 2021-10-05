fecf6db71ed34dbf442e7b2e8655bd30a6682371
RemyDegenne <Remydegenne@gmail.com>
2021-10-05 18:05:15 +0200

    fix import

--- src/topology/metric_space/hausdorff_dimension.lean
+++ src/topology/metric_space/hausdorff_dimension.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Yury Kudryashov
 -/
 import measure_theory.measure.hausdorff
+import analysis.calculus.times_cont_diff
 
 /-!
 # Hausdorff dimension
